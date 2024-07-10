da98079e800e477887d7fc5857d51ab380a742b9
josungwong <jsw13160@gmail.com>
2024-07-10 23:29:26 +0900

    11시 30분

---
+++ main.js
@@ -0,0 +1,87 @@
+const apiKey = `fa80bc1d486a4ac3b407ca0ee368b453`
+let COUNTRY = "us"
+let CateGory = ""
+let topButton = document.querySelectorAll(".menus button")
+let newsList = []
+topButton.forEach((menu)=>
+    menu.addEventListener("click",async (event)=>{
+        if(event){
+            CateGory = event.target.id
+        }
+        console.log(CateGory)
+        const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=${COUNTRY}&category=${CateGory}&apiKey=${apiKey}`)
+        everyTime(url)
+        console.log("카테고리")
+    }))
+
+const getLatestNews = async()=>{ // async: 동기함수로 만들기 (await으로 기다리게 만들수 있음)
+
+    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=${COUNTRY}&apiKey=${apiKey}`) // URL: 인스턴스, 미리 필요한걸 해주는거
+
+    //https://newsapi.org/v2/top-headlines?country=${COUNTRY}&apiKey=${apiKey} 너무 많이 쓰다보니까 요구량이 너무 많다고 하면서 사용이 안되네요ㅠ
+
+    everyTime(url)
+    console.log("가장 처음")
+}
+const everyTime = async(url) => {
+
+    const response = await fetch(url)
+
+    const data = await response.json()
+
+    newsList = data.articles
+    render()
+    console.log("리스트출력: ", newsList)
+}
+const getNewsByKeyWord = async() => {
+        const keyword = document.getElementById("search-input").value
+    console.log(keyword)
+    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=${COUNTRY}&q=${keyword}&apiKey=${apiKey}`)
+    everyTime(url)
+    console.log("키워드")
+} 
+
+const render = () =>{
+    const newsHTML = newsList.map(news=>`<div class="row news">
+        <div class="col-lg-4">
+        <img class="news-img-size" src=${news.urlToImage} onerror="this.onerror=null; this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUnvISVTYopMAy17o3mB2lfSPeEjoKfAdV2w&s';">
+    </div>
+    <div class="col-lg-8">
+        <h2>${news.title}</h2>
+        <p>${overTwoH(news)}</p>
+        <div>${noSource(news)} * ${coolTime(news)}</div>
+    </div>
+    </div>`).join('')
+
+    document.getElementById('news-board').innerHTML = newsHTML
+}
+const overTwoH = (news)=> {
+    let arr = ""
+    arr = news.description
+    if(arr == null || arr == "[Removed]" || arr == ""){
+        return arr = "내용없음"
+    } else if (arr.length > 200){
+        return arr.substring(0,200) + "..."
+    } else{
+        return arr
+    }
+}
+const noSource= (news) =>{
+    let arr1 = ""
+    arr1 = news.source.name
+    if(arr1.length <= 0){
+        arr1 = "no source"
+        return arr1
+    } else{
+        return arr1
+    }
+} 
+const coolTime = (news) => {
+    let date = moment(news.publishedAt.substring(0,10),"YYYYMMDD").fromNow()
+    let hour = moment(news.publishedAt.substring(11,19),"h:mm:ss").fromNow()
+
+    return date[0]+date[1] +"days  "+ hour
+}
+getLatestNews()
+
+
